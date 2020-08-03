5e0524ee32f677ae15c00e075bed3169eb01a159
Artyom Zborovsky <artem.zborovsky2@gmail.com>
2020-08-03 12:39:59 +0300

    added webpack to the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/script.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    task1: ['@babel/polyfill/noConflict', './script.js'],
+    task2: ['@babel/polyfill/noConflict', './script_2.js'],
+    task3: ['@babel/polyfill/noConflict', './script_3.js'],
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 4200,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
